f2f6644429190a4d754b05709223374e330d21f1
Martin Ansong <martin.ansong@harness.io>
2025-09-01 21:41:34 -0400

    feat: add unique bean name for version controller to prevent autowiring conflicts

--- app/src/main/java/com/stocktrack/web/VersionController.java
+++ app/src/main/java/com/stocktrack/web/VersionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestController("versionInfoController")
 public class VersionController {
 
     private final ObjectProvider<BuildProperties> buildPropertiesProvider;
